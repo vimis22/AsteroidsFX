2896e622672bc895c56735d4d5171a9f69a358e3
vimis22 <vimis22@student.sdu.dk>
2024-02-26 22:56:30 +0100

    I have just created the Enemy. Now I just need to define how the enemy is going to move around in the game.

--- Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/EnemyControlSystem.java
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/EnemyControlSystem.java
@@ -1,12 +1,19 @@
 package dk.sdu.mmmi.cbse.enemy;
 
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.GameKeys;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 
 public class EnemyControlSystem implements IEntityProcessingService {
     @Override
     public void process(GameData gameData, World world) {
-
+        for (Entity enemy : world.getEntities(Enemy.class) ){
+            double changeX = Math.cos(Math.toRadians(enemy.getRotation()));
+            double changeY = Math.sin(Math.toRadians(enemy.getRotation()));
+            enemy.setX(enemy.getX() + changeX);
+            enemy.setY(enemy.getY() + changeY);
+        }
     }
 }
--- Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/EnemyPlugin.java
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/EnemyPlugin.java
@@ -1,18 +1,43 @@
 package dk.sdu.mmmi.cbse.enemy;
 
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
 public class EnemyPlugin implements IGamePluginService {
+    private Entity enemy;
 
-    @Override
-    public void start(GameData gameData, World world) {
+    public EnemyPlugin() {
 
     }
 
     @Override
-    public void stop(GameData gameData, World world) {
+    public void start(GameData gameData, World world) {
+        enemy = createEnemy(gameData);
+        world.addEntity(enemy);
+    }
 
+    private Entity createEnemy(GameData gameData){
+        Entity enemyShip = new Enemy();
+        enemyShip.setPolygonCoordinates(
+                0,0,
+                0,5,
+                5,10,
+                10,10,
+                15,5,
+                15,0,
+                10,-5,
+                5,-5,
+                0,0
+        );
+        enemyShip.setX(gameData.getDisplayHeight()/2);
+        enemyShip.setY(gameData.getDisplayWidth()/2);
+        return enemyShip;
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        world.removeEntity(enemy);
     }
 }
