b5d44d879ae71fdcafb285dfadc7b79d839ead17
vimis22 <vimis22@student.sdu.dk>
2024-02-26 22:02:29 +0100

    The file structure of Enemy has been created along with all the necessary files.
    Now the system implementation needs to take place.

---
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/EnemyPlugin.java
@@ -0,0 +1,18 @@
+package dk.sdu.mmmi.cbse.enemy;
+
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+public class EnemyPlugin implements IGamePluginService {
+
+    @Override
+    public void start(GameData gameData, World world) {
+
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+
+    }
+}
--- Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/Main.java
+++
@@ -1,7 +0,0 @@
-package dk.sdu.mmmi.cbse.enemy;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
