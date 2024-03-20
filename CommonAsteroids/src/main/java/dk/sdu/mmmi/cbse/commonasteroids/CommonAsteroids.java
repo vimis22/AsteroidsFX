411b499754f5d4bd2d64beb444147835d6d334bb
vimis22 <vimis22@student.sdu.dk>
2024-03-20 11:59:53 +0100

    I have just rebased the split_asteroids_branch on main

---
+++ CommonAsteroids/src/main/java/dk/sdu/mmmi/cbse/commonasteroids/CommonAsteroids.java
@@ -0,0 +1,8 @@
+package dk.sdu.mmmi.cbse.commonasteroids;
+
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+public class CommonAsteroids implements IGamePluginService, IEntityProcessingService {
+
+}
