d9a8f2089737f55224a7446d19d46b410c4a3a60
vimis22 <vimis22@student.sdu.dk>
2024-04-21 13:23:36 +0200

    Working on AnnotationsContext.

---
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/HelperClass.java
@@ -0,0 +1,36 @@
+package dk.sdu.mmmi.cbse.main;
+
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.ServiceLoader;
+
+import static java.util.stream.Collectors.toList;
+import java.util.List;
+
+
+public class HelperClass {
+
+    public HelperClass(){
+
+    }
+
+    public Game game(){
+        return new Game(getPluginServices(),getEntityProcessingServices(),getPostEntityProcessingServices());
+    }
+
+    private List<? extends IGamePluginService> getPluginServices() {
+        return ServiceLoader.load(IGamePluginService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+    }
+
+    private List<? extends IEntityProcessingService> getEntityProcessingServices() {
+        return ServiceLoader.load(IEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+    }
+
+    private List<? extends IPostEntityProcessingService> getPostEntityProcessingServices() {
+        return ServiceLoader.load(IPostEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+    }
+}
