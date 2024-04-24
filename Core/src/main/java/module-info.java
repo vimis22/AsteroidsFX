7de118ac1256bc18a6bc65f722745eb918ea551b
vimis22 <vimis22@student.sdu.dk>
2024-04-24 12:04:49 +0200

    Experimenting some stuff to make ApplicationsContext work.

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -5,7 +5,8 @@ module Core {
     requires spring.context;
     requires spring.core;
     requires spring.beans;
-    opens dk.sdu.mmmi.cbse.main to javafx.graphics;
+    exports dk.sdu.mmmi.cbse.main;
+    opens dk.sdu.mmmi.cbse.main to javafx.graphics,spring.core;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
