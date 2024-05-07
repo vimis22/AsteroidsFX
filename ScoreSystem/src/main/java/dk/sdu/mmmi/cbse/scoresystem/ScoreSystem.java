981f99df3f194b0efa644524ff3cccb030c90e98
vimis22 <vimis22@student.sdu.dk>
2024-05-07 20:20:51 +0200

    Scoresystem has got the connection of SpringBootApplication, after adding a version to javafx-controls version inside pom.xml.

---
+++ ScoreSystem/src/main/java/dk/sdu/mmmi/cbse/scoresystem/ScoreSystem.java
@@ -0,0 +1,22 @@
+package dk.sdu.mmmi.cbse.scoresystem;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ScoreSystem {
+    private Long totalScore = 0L;
+
+    public static void main(String[] args){
+        SpringApplication.run(ScoreSystem.class, args);
+    }
+
+    @GetMapping("/score")
+    public Long calculateHealth(@RequestParam(value="point") Long point) {
+        totalScore += point;
+        return totalScore;
+    }
+}
---
+++ ScoreSystem/src/test/java/ScoreSystemTest.java
@@ -0,0 +1,13 @@
+package dk.sdu.mmmi.cbse.scoresystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ScoreSystemTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
